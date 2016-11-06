ab9ed376c01c23e5b8e6001d766cf2a312503848
kidd5478 <mkidd@hawk.iit.edu>
2016-11-06 15:55:56 -0500

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,5 +1,18 @@
 <?
+<?php
+$servername = "mydbinstancefixed";
+$username = "masterawsuser";
+$password = "masteruserpassword ";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+?>
 
 
 ?>
