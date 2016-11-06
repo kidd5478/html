c03d8b61c97c6921f1021c426551c87b92976246
kidd5478 <mkidd@hawk.iit.edu>
2016-11-06 14:27:55 -0500

    Create s3test.php

---
+++ s3test.php
@@ -0,0 +1,13 @@
+<?
+php
+?>
+
+<html>
+<head>
+<meta name="viewport" content="width=device-width, minimum-scale=0.1">
+<title>switchonarex.png (3677×2400)</title>
+</head>
+<body style="margin: 0px;">
+<img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/mk-trex/img/switchonarex.png" width="802" height="523">
+</body>
+</html>
