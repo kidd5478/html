5f390937aeac75e340f17bef2262bd985d263389
kidd5478 <mkidd@hawk.iit.edu>
2016-11-20 01:47:45 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -4,7 +4,7 @@ session_start();
 
 echo "Your username is: " .  $_SESSION['username'];
 $username = $_SESSION['username'];
-echo "\n" . md5($username);
+
 
 $_SESSION['receipt'] = md5($username);
 ?>
@@ -12,14 +12,22 @@ $_SESSION['receipt'] = md5($username);
 <html>
 <head><title>Gallery Page</title>
 </head>
-<body>
-Gallery PHP
+<body><br/>
+Gallery PHP<br/>
 <a href="gallery.php"> Gallery </a> <a href="upload.php"> Upload </a>
-
+<br />
   <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/raw-mak/Knuth-bw.jpg" width="200" height="200">
-  <br/ >
+  &nbsp;
   <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/raw-mak/eartrumpet-bw.png" width="200" height="200">
-  <br/ >
+  &nbsp;
   <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/raw-mak/mountain-bw.jpg" width="200" height="200">
-
+<br/>
+  
+  <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/finished-mak/Knuth.jpg" width="200" height="200">
+  &nbsp;
+  <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/finished-mak/eartrumpet.png" width="200" height="200">
+  &nbsp;
+  <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/finished-mak/mountain.jpg" width="200" height="200">
+  
+  
 </body></html>
