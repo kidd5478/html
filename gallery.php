7244e648888e48341495c590b0983f76f77a1b3f
kidd5478 <mkidd@hawk.iit.edu>
2016-11-20 01:30:02 -0500

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -2,18 +2,24 @@
 
 session_start();
 
-echo "Your email is: " .  $_SESSION['email'];
-$email = $_SESSION['email'];
-echo "\n" . md5($email);
+echo "Your username is: " .  $_SESSION['username'];
+$username = $_SESSION['username'];
+echo "\n" . md5($username);
 
-$_SESSION['receipt'] = md5($email);
+$_SESSION['receipt'] = md5($username);
 ?>
 
 <html>
-<head><title>Hello app</title>
+<head><title>Gallery Page</title>
 </head>
 <body>
 Gallery PHP
 <a href="gallery.php"> Gallery </a> <a href="upload.php"> Upload </a>
+
+  <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/raw-mak/img/Knuth-bw.jpg.png" width="200" height="200">
+  <br/ >
+  <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/raw-mak/img/eartrumpet-bw.png" width="200" height="200">
+  <br/ >
+  <img style="-webkit-user-select: none; cursor: zoom-in;" src="https://s3-us-west-2.amazonaws.com/raw-mak/img/mountain-bw.jpg" width="200" height="200">
 
 </body></html>
