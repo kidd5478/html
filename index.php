d6c8ab041a79f57d1a698f2617813ac1b807889a
kidd5478 <mkidd@hawk.iit.edu>
2016-11-20 14:58:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo "Hello World \n";
 require 'vendor/autoload.php';
 $s3 = new Aws\S3\S3Client([
     'version' => 'latest',
-    'region'  => 'us-west-1'
+    'region'  => 'us-west-2'
 ]);
 // Use an Aws\Sdk class to create the S3Client object.
 $result = $s3->listBuckets();
