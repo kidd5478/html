af9f7c727c401ab591c2df4f01c9001fab14cf12
kidd5478 <mkidd@hawk.iit.edu>
2016-11-20 17:04:42 -0500

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,10 @@
+<html>
+<head><title>Welcome Page</title>
+</head>
+<body>
+<hr />
+<p> I am not quite sure how to do this step. If I were in Chicago I would come and ask you for help as I did in the past for a crash course
+in this area. I want to specialize in System Administration although I do like all aspects of computing.</p>
+<a href="gallery.php"> Gallery </a> | <a href="upload.php"> Upload </a>
+
+</body></html>
